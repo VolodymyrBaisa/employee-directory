dab741c8f0010963360a2b5c2687a9e1507b50f8
Volodymyr Baisa <biosvova@gmail.com>
2020-12-22 19:43:08 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header.js";
+import Body from "./components/Body.js";
+
+function App() {
+    return (
+        <>
+            <Header />
+            {/* <Body /> */}
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Logo from "./Logo.js";
+
+function Header() {
+    return <Logo />;
+}
+
+export default Header;
